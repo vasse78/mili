2b49dca5fa9e6f746e292e451331c797fe99e2e6
sergey <0404pmm@mail.ru>
2023-03-12 19:44:24 +0300

    fist commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double price=10_000.99;
+        int cost=20;
+       int bonus= (int) (price/cost);
+
+
+        System.out.println("Количество миль ="+bonus);}
+
+}
